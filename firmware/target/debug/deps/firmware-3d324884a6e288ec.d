1e7d53f0616a2d11e5aaa4bc2e9d4d121a680b15
Sergei Lavrentev <23312691+lavrd@users.noreply.github.com>
2024-04-12 22:26:16 +0400

    Add utils for Rust projects

--- firmware/target/debug/deps/firmware-3d324884a6e288ec.d
+++
@@ -1,5 +0,0 @@
-/Users/lavr/dev/github/vts/firmware/target/debug/deps/libfirmware-3d324884a6e288ec.rmeta: src/main.rs
-
-/Users/lavr/dev/github/vts/firmware/target/debug/deps/firmware-3d324884a6e288ec.d: src/main.rs
-
-src/main.rs:
