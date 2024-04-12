fb3d14aeed8f4b52bb202b1b40ab775a5c22addd
Sergei Lavrentev <23312691+lavrd@users.noreply.github.com>
2024-04-12 22:21:35 +0400

    Init firmware folder

---
+++ firmware/target/debug/deps/firmware-3d324884a6e288ec.d
@@ -0,0 +1,5 @@
+/Users/lavr/dev/github/vts/firmware/target/debug/deps/libfirmware-3d324884a6e288ec.rmeta: src/main.rs
+
+/Users/lavr/dev/github/vts/firmware/target/debug/deps/firmware-3d324884a6e288ec.d: src/main.rs
+
+src/main.rs:
---
+++ firmware/target/debug/deps/firmware-c41e39417c160feb.d
@@ -0,0 +1,5 @@
+/Users/lavr/dev/github/vts/firmware/target/debug/deps/libfirmware-c41e39417c160feb.rmeta: src/main.rs
+
+/Users/lavr/dev/github/vts/firmware/target/debug/deps/firmware-c41e39417c160feb.d: src/main.rs
+
+src/main.rs:
