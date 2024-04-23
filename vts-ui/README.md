de3a72d2c040602cb23ffd9dd594425400733664
Sergei Lavrentev <23312691+lavrd@users.noreply.github.com>
2024-04-23 22:03:28 +0400

    Trying to use actor to interact with canister

--- vts-ui/README.md
+++ vts-ui/README.md
@@ -5,9 +5,9 @@
 Prepare `.env` file before run:
 
 ```.env
-VITE_INTERNET_IDENTITY_CANISTER_ENDPOINT=http://bd3sg-teaaa-aaaaa-qaaba-cai.localhost:7777
-VITE_ICP_NODE_ENDPOINT=http://127.0.0.1:7777
-VITE_VTS_CANISTER_ID=bkyz2-fmaaa-aaaaa-qaaaq-cai
+VITE_INTERNET_IDENTITY_CANISTER_ENDPOINT=http://bkyz2-fmaaa-aaaaa-qaaaq-cai.localhost:7777/
+VITE_ICP_NODE_ENDPOINT=http://localhost:7777
+VITE_VTS_CANISTER_ID=bd3sg-teaaa-aaaaa-qaaba-cai
 ```
 
 Then:
--- vts-ui/src/views/FirmwaresView.vue
+++ vts-ui/src/views/FirmwaresView.vue
@@ -2,10 +2,31 @@
 import { initVTSClient } from '@/icp'
 
 export default {
+  data() {
+    return {
+      loading: false,
+      success: '',
+      error: ''
+    }
+  },
   methods: {
     async request() {
+      if (this.loading) {
+        console.log("..")
+        return
+      }
+      this.loading = true;
+      this.success = '';
+      this.error = '';
       const vtsClient = await initVTSClient()
+      console.log(vtsClient)
+      console.log(vtsClient.request_firmware)
       const res = await vtsClient.request_firmware()
+      console.log(res)
+      console.log(res.Ok)
+      if (res.Ok === null) this.success = "Successfully requested new firmware!";
+      if (res.Err && res.Err.AlreadyExists === null) this.error = 'You already have active firmware request.';
+      this.loading = false
     },
   },
 }
@@ -13,7 +34,16 @@ export default {
 
 <template>
   <h1>Firmwares</h1>
-  <button type="button" @click="request">Request</button>
+  <button type="button" @click="request">
+    <span v-if="!loading">Request</span>
+    <div class="loader" v-if="loading"></div>
+  </button>
+  <div>
+    <p class="success alert" v-if="success !== ''">{{ success }}</p>
+  </div>
+  <div>
+    <p class="error alert" v-if="error !== ''">{{ error }}</p>
+  </div>
 </template>
 
 <style scoped></style>
