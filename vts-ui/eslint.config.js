3ef2e1934ab8fa851fd3feab4a2805466e82c458
Sergei Lavrentev <23312691+lavrd@users.noreply.github.com>
2024-06-05 13:22:27 +0400

    Update frontend dependencies

--- vts-ui/eslint.config.js
+++ vts-ui/eslint.config.js
@@ -1,14 +1,14 @@
-import js from "@eslint/js";
-import pluginVue from 'eslint-plugin-vue';
+import js from '@eslint/js'
+import pluginVue from 'eslint-plugin-vue'
 export default [
   js.configs.recommended,
   ...pluginVue.configs['flat/recommended'],
   {
     rules: {
-      "no-unused-vars": "warn"
+      'no-unused-vars': 'error',
     },
   },
   {
-    ignores: ["src/assets/declarations/**"]
-  }
-];
+    ignores: ['src/assets/declarations/**'],
+  },
+]
