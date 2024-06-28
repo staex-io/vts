0a97718d523bbd3b7b8b1be9157b6c393487c922
Sergei Lavrentev <23312691+lavrd@users.noreply.github.com>
2024-06-28 14:59:27 +0400

    Move icp ledger canister id to the .env

--- vts-ui/src/icp_ledger.js
+++ vts-ui/src/icp_ledger.js
@@ -5,11 +5,11 @@ import { LedgerCanister, AccountIdentifier, SubAccount } from '@dfinity/ledger-i
 export const ICPLedgerClient = async (authClient) => {
   const agent = await createAgent({
     identity: authClient._identity,
-    host: 'http://127.0.0.1:7777',
+    host: import.meta.env.VITE_ICP_NODE_ENDPOINT,
   })
   const icpLedgerClient = LedgerCanister.create({
     agent,
-    canisterId: 'ryjl3-tyaaa-aaaaa-aaaba-cai',
+    canisterId: import.meta.env.VITE_ICP_LEDGER_CANISTER_ID,
   })
   return icpLedgerClient
 }
